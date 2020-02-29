960e56985e73fd61a35697f63194d2c1ec5670d6
Victor Cruz <vicdan215@gmail.com>
2020-02-29 00:12:15 -0600

    Adjusting main formats & comments on inserts

--- main.c
+++ main.c
@@ -3,7 +3,6 @@
  * main.c
  */
 
-
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -12,12 +11,10 @@
 
 static FILE *traceFile;
 
-
-int main(argc, argv)
-  int argc;
-  char **argv;
+int main(argc, argv) int argc;
+char **argv;
 {
-  // Lectura de los argumentos de la línea de comando y establece los parámetros de la memoria cache 
+  // Lectura de los argumentos de la línea de comando y establece los parámetros de la memoria cache
   parse_args(argc, argv);
   // Inicializa la memoria cache
   init_cache();
@@ -26,7 +23,6 @@ int main(argc, argv)
   // Imprime los resultados estadísticos de la simulación el cache
   print_stats();
 }
-
 
 /************************************************************/
 /*
@@ -44,14 +40,14 @@ que se quiere simular. Entre los diferentes argumentos están:
 * -wa: establece la política de alocación de memoria a write-allocate
 * -nw: establece la política de alocación de memoria a no-write-allocate 
 */
-void parse_args(argc, argv)
-  int argc;
-  char **argv;
+void parse_args(argc, argv) int argc;
+char **argv;
 {
   int arg_index, i, value;
 
   // explica al usuario de la línea de comando como llamar al programa
-  if (argc < 2) {
+  if (argc < 2)
+  {
     printf("usage:  sim <options> <trace file>\n");
     exit(-1);
   }
@@ -60,7 +56,8 @@ void parse_args(argc, argv)
   // primero busca para ver si entre toda la línea de argumentos pasados
   // no hay una llamada a -h, si lo hay sale de la ejecución con exit(0)
   for (i = 0; i < argc; i++)
-    if (!strcmp(argv[i], "-h")) {
+    if (!strcmp(argv[i], "-h"))
+    {
       printf("\t-h:  \t\tthis message\n\n");
       printf("\t-bs <bs>: \tset cache block size to <bs>\n");
       printf("\t-us <us>: \tset unified cache size to <us>\n");
@@ -73,67 +70,77 @@ void parse_args(argc, argv)
       printf("\t-nw: \t\tset allocation policy to no write allocate\n");
       exit(0);
     }
-    
+
   arg_index = 1;
   // ojo: vamos hasta argc - 1 porque el úlimo elemento debe ser el archivo *.trace
-  while (arg_index != argc - 1) {
+  while (arg_index != argc - 1)
+  {
 
     /* set the cache simulator parameters */
 
-    if (!strcmp(argv[arg_index], "-bs")) {
-      value = atoi(argv[arg_index+1]);
+    if (!strcmp(argv[arg_index], "-bs"))
+    {
+      value = atoi(argv[arg_index + 1]);
       set_cache_param(CACHE_PARAM_BLOCK_SIZE, value);
       arg_index += 2;
       continue;
     }
 
-    if (!strcmp(argv[arg_index], "-us")) {
-      value = atoi(argv[arg_index+1]);
+    if (!strcmp(argv[arg_index], "-us"))
+    {
+      value = atoi(argv[arg_index + 1]);
       set_cache_param(CACHE_PARAM_USIZE, value);
       arg_index += 2;
       continue;
     }
 
-    if (!strcmp(argv[arg_index], "-is")) {
-      value = atoi(argv[arg_index+1]);
+    if (!strcmp(argv[arg_index], "-is"))
+    {
+      value = atoi(argv[arg_index + 1]);
       set_cache_param(CACHE_PARAM_ISIZE, value);
       arg_index += 2;
       continue;
     }
 
-    if (!strcmp(argv[arg_index], "-ds")) {
-      value = atoi(argv[arg_index+1]);
+    if (!strcmp(argv[arg_index], "-ds"))
+    {
+      value = atoi(argv[arg_index + 1]);
       set_cache_param(CACHE_PARAM_DSIZE, value);
       arg_index += 2;
       continue;
     }
 
-    if (!strcmp(argv[arg_index], "-a")) {
-      value = atoi(argv[arg_index+1]);
+    if (!strcmp(argv[arg_index], "-a"))
+    {
+      value = atoi(argv[arg_index + 1]);
       set_cache_param(CACHE_PARAM_ASSOC, value);
       arg_index += 2;
       continue;
     }
 
-    if (!strcmp(argv[arg_index], "-wb")) {
+    if (!strcmp(argv[arg_index], "-wb"))
+    {
       set_cache_param(CACHE_PARAM_WRITEBACK, value);
       arg_index += 1;
       continue;
     }
 
-    if (!strcmp(argv[arg_index], "-wt")) {
+    if (!strcmp(argv[arg_index], "-wt"))
+    {
       set_cache_param(CACHE_PARAM_WRITETHROUGH, value);
       arg_index += 1;
       continue;
     }
 
-    if (!strcmp(argv[arg_index], "-wa")) {
+    if (!strcmp(argv[arg_index], "-wa"))
+    {
       set_cache_param(CACHE_PARAM_WRITEALLOC, value);
       arg_index += 1;
       continue;
     }
 
-    if (!strcmp(argv[arg_index], "-nw")) {
+    if (!strcmp(argv[arg_index], "-nw"))
+    {
       set_cache_param(CACHE_PARAM_NOWRITEALLOC, value);
       arg_index += 1;
       continue;
@@ -141,7 +148,6 @@ void parse_args(argc, argv)
 
     printf("error:  unrecognized flag %s\n", argv[arg_index]);
     exit(-1);
-
   }
 
   dump_settings();
@@ -150,7 +156,7 @@ void parse_args(argc, argv)
   // cuando sale del ciclo while, arg_index es el índice
   // del elemento donde debe leer la dirección del archivo
   // *.trace, recordar que traceFile esta declarado de forma
-  // global por lo que no hace falta regresar nada como 
+  // global por lo que no hace falta regresar nada como
   // resultado de la función
   traceFile = fopen(argv[arg_index], "r");
 
@@ -161,7 +167,7 @@ void parse_args(argc, argv)
 /************************************************************/
 
 void play_trace(inFile)
-  FILE *inFile;
+    FILE *inFile;
 {
   unsigned addr, data, access_type;
   int num_inst;
@@ -171,12 +177,14 @@ void play_trace(inFile)
   // la función read_trace_element regresa 0 cuando se alcanza
   // el final (EOF) del archivo leído. Por eso se puede utilizar
   // al interior de un while
-  while(read_trace_element(inFile, &access_type, &addr)) {
+  while (read_trace_element(inFile, &access_type, &addr))
+  {
 
     // los valors de access type están definidos en main.h
     // estas instrucciones se ejecutan por cada línea de los
     // archivos *.trace
-    switch (access_type) {
+    switch (access_type)
+    {
     case TRACE_DATA_LOAD:
     case TRACE_DATA_STORE:
     case TRACE_INST_LOAD:
@@ -199,8 +207,8 @@ void play_trace(inFile)
 /************************************************************/
 // esta función lee una línea a la vez de los archivos *.trace
 int read_trace_element(inFile, access_type, addr)
-  FILE *inFile;
-  unsigned *access_type, *addr;
+    FILE *inFile;
+unsigned *access_type, *addr;
 {
   int result;
   char c;
@@ -210,14 +218,15 @@ int read_trace_element(inFile, access_type, addr)
   // "2 408ed4" por lo que el %c corresponde a \n o espacios
   // que se eliminan en el ciclo while interno
   result = fscanf(inFile, "%u %x%c", access_type, addr, &c);
-  while (c != '\n') {
+  while (c != '\n')
+  {
     result = fscanf(inFile, "%c", &c);
-    if (result == EOF) 
+    if (result == EOF)
       break;
   }
   if (result != EOF)
-    return(1);
+    return (1);
   else
-    return(0);
+    return (0);
 }
 /************************************************************/
--- main.h
+++ main.h
@@ -3,7 +3,6 @@
  * main.h
  */
 
-
 #define TRACE_DATA_LOAD 0
 #define TRACE_DATA_STORE 1
 #define TRACE_INST_LOAD 2
@@ -13,4 +12,3 @@
 void parse_args();
 void play_trace();
 int read_trace_element();
-
