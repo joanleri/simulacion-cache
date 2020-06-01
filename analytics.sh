c3902afd56e1d2f37091478e75daac522d2bd072
Victor Cruz <vicdan215@gmail.com>
2020-05-31 19:21:30 -0500

    New

--- analytics.sh
+++ analytics.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-echo "index, split I cache, split D cache, unified cache, assoc, block size, write, allocation, inst acc, inst mis, inst miss rate, inst hit rate, inst replace, data acc, data mis, data miss rate, data hit rate, data replace, demand fetch, copies back"
+echo "archivo, index, split I cache, split D cache, unified cache, assoc, block size, write, allocation, inst acc, inst mis, inst miss rate, inst hit rate, inst replace, data acc, data mis, data miss rate, data hit rate, data replace, demand fetch, copies back"
 
-for i in `seq 1 10`;
+for f in "trazas/spice.trace" "trazas/cc.trace" "trazas/tex.trace";
 do
-    # echo "Iniciando prueba" $i $'\n'
-    size=`expr $((2**$i)) \* 1024`
-    res=$(./sim.exe -bs 16 -is $size -ds $size -a 1 -wb -wa  trazas/spice.trace)
-    # res=$(ls -la nofile 2>&1)
-    echo $i,$res
+    for i in `seq 2 12`;
+    do
+        # echo "Iniciando prueba" $i $'\n'
+        size=`expr $((2**$i))`
+        # assoc=`expr $((2**$i))`
+        res=$(./sim.exe -bs $size -is 8192 -ds 8192 -a 2 -wb -wa  $f)
+        echo $f,$i,$res
+    done
 done
